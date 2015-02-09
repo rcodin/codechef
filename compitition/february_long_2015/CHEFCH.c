7d24053c5feef225ee6728f9a2f940aa54a0eefe
rcodin <ronit.codin@gmail.com>
2015-02-09 15:18:05 +0530

    The Chef and Chain solved with better result

--- compitition/february_long_2015/CHEFCH.c
+++ compitition/february_long_2015/CHEFCH.c
@@ -2,39 +2,45 @@
 #include <stdio.h>
 #include <string.h>
 int main(){
-	long int T,i,j,len;
-	char S[100001],cnt1,cnt2,tmp[100001];
+	int T,i,j,len,cnt1,cnt2;
+	char S[100001],p,c;
 	scanf("%ld",&T);
 	while(T--){
 		scanf("%s",S);
 		len=strlen(S);
-		strcpy(tmp,S);
+		// strcpy(tmp,S);
 		cnt1=0;
-		if(tmp[0]=='-'){
+		p=S[0];
+		if(S[0]=='-'){
 			cnt1=1;
-			tmp[0]='+';
+			p='+';
 		}
 		for(i=0;i<len-1;i++){
-			if((tmp[i]==tmp[i+1])){
+			c=S[i+1];
+			if(p==c){
 				cnt1++;
-				if(tmp[i]=='+') tmp[i+1]='-';
-				else tmp[i+1]='+';
+				if(c=='+') c='-';
+				else c='+';
 			}
+			p=c;
 		}
-		strcpy(tmp,S);
+		// strcpy(tmp,S);
 		cnt2=0;
-		if(tmp[0]=='+'){
+		p=S[0];
+		if(S[0]=='+'){
 			cnt2=1;
-			tmp[0]='-';
+			p='-';
 		}
 		for(i=0;i<len-1;i++){
-			if((tmp[i]==tmp[i+1])){
+			c=S[i+1];
+			if(p==c){
 				cnt2++;
-				if(tmp[i]=='+') tmp[i+1]='-';
-				else tmp[i+1]='+';
+				if(c=='+') c='-';
+				else c='+';
 			}
+			p=c;
 		}
-		printf("%d\n",(cnt1>cnt2)?cnt2:cnt1);
+		printf("%ld\n",(cnt1>cnt2)?cnt2:cnt1);
 	}
 	return 0;
 }
